12e27bbde70ac30fc109813eff697db467724203
Stian Gaustad <stiankg@stud.ntnu.no>
2021-12-24 11:56:23 +0100

    UI - Checkstyle and spotbugs fix

--- grades/ui/src/main/java/grades/ui/ApplicationMyGrades.java
+++ grades/ui/src/main/java/grades/ui/ApplicationMyGrades.java
@@ -9,19 +9,19 @@ import javafx.stage.Stage;
 
 public class ApplicationMyGrades extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		Parent parent = FXMLLoader.load(
-				getClass().getResource("PrimaryGUI.fxml"));
-		primaryStage.setTitle("Stian K. Gaustad TDT4100 Project - My Grades");
-		primaryStage.getIcons().add(
-				new Image(getClass().getResourceAsStream(
-						"images/testThumbnail.jpg")));
-		primaryStage.setScene(new Scene(parent));
-		primaryStage.show();
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(
+                getClass().getResource("PrimaryGUI.fxml"));
+        primaryStage.setTitle("Stian K. Gaustad TDT4100 Project - My Grades");
+        primaryStage.getIcons().add(
+                new Image(getClass().getResourceAsStream(
+                        "images/testThumbnail.jpg")));
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
+    }
 
-	public static void main(String[] args) {
-		launch(ApplicationMyGrades.class, args);
-	}
+    public static void main(String[] args) {
+        launch(ApplicationMyGrades.class, args);
+    }
 }
